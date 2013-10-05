59a312967e71e5f27dcc12734e4fd6125942dbd0
Christian Fleschhut <christianfleschhut@gmail.com>
2013-10-05 23:08:00 +0200

    refactor book_spec w/ subject & let

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,37 +1,11 @@
 require 'spec_helper'
 
 describe Book do
+  let(:book) { Book.new "title", "author", :category }
+  subject { book }
 
-  before :each do
-    @book = Book.new "title", "author", :category
-  end
-
-  describe "#new" do
-    # it "return a new book object" do
-    #   @book.should be_an_instance_of Book
-    # end
-
-    it "takes three paramters and returns a book object" do
-      @book.should be_an_instance_of Book
-    end
-  end
-
-  describe "#title" do
-    it "returns the correct title" do
-      @book.title.should eql "title"
-    end
-  end
-
-  describe "#author" do
-    it "returns the correct author" do
-      @book.author.should eql "author"
-    end
-  end
-
-  describe "#category" do
-    it "returns the correct category" do
-      @book.category.should eql :category
-    end
-  end
-
+  it { should be_an_instance_of Book }
+  its(:title) { should eql "title" }
+  its(:author) { should eql "author" }
+  its(:category) { should eql :category }
 end
